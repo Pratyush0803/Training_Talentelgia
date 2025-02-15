56326761115233dd009f8e72ad5b92a3ae5c021e
Pratyush0803 <jenapratyush29@gmil.com>
2025-02-15 22:22:01 +0530

    Day3

---
+++ Week-1/DAy -3/Kotlin/OOPs_Practice_Classes_Objects.kt
@@ -0,0 +1,56 @@
+// Classes and Objects
+//Create a Car class with properties brand, model, and year. Instantiate an object and print its details.
+//class Car {
+//    var brand:String = ""
+//    var model:String = ""
+//    var year:Int = 0
+//}
+//fun main() {
+//    val myCar = Car()
+//    myCar.brand = "Bajaj"
+//    myCar.model = "Discover 100"
+//    myCar.year = 2015
+//    println("Car Brand is ${myCar.brand}")
+//    println("Car Model is ${myCar.model}")
+//    println("Car Manufacture date  is ${myCar.year}")
+//}
+
+//Write a BankAccount class with deposit() and withdraw() functions. Ensure balance cannot go negative.
+//class BankAccount {
+//    fun deposit(amount:Double) {
+//        if (amount >= 0){
+//            println("$amount Amount Deposited to Account")
+//        }else
+//            println("Invalid Amount")
+//    }
+//    fun withdraw(amount:Double) {
+//        if (amount >= 0){
+//            println("$amount Amount Withdrawn")
+//        }
+//        else
+//            println("Invalid Amount")
+//    }
+//}
+//
+//fun main() {
+//    val myAccount = BankAccount()
+//    myAccount.deposit(1001.0)
+//    myAccount.withdraw(1000.0)
+//}
+
+//Implement a Rectangle class with length and width properties, and a function to calculate area.
+//class Rectangle {
+//    var length:Int = 0
+//    var width:Int = 0
+//    fun area(){
+//        val area = length * width
+//        println("The area Rectangle is : $area square unit")
+//    }
+//}
+//
+//fun main() {
+//    val areaRectangle = Rectangle()
+//    areaRectangle.length = 40
+//    areaRectangle.width = 40
+//    areaRectangle.area()
+//}
---
+++ Week-1/DAy -3/Kotlin/OOPs_Practice_Encapsulation.kt
@@ -0,0 +1,23 @@
+//Create a User class with a private password field and provide getter and setter methods.
+//class User {
+//    private var password: String = ""
+//    fun setPassword(pass: String)
+//    {
+//        password = pass
+//    }
+//
+//    fun getPassword(): String {
+//        return password
+//    }
+//}
+//
+//fun main() {
+//    val myPassword = User()
+//    myPassword.setPassword("Bubu@123")
+//    println(myPassword.getPassword())
+//}
+
+
+
+
+
